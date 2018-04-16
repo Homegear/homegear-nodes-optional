4adaa96c37d82e8a220bec1eeb766b290574f005
Sathya Laufer <sathya@laufers.net>
2018-04-16 17:31:16 +0200

    Initial commit

---
+++ ftp/CreateDebianPackage.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+if test -z version.txt; then
+    echo "Please create a version file."
+    exit 0;
+fi
+if test -z revision.txt; then
+    echo "Please create a revision file."
+    exit 0;
+fi
+if test -z $1; then
+    echo "Please specify a distribution (i. e. wheezy)."
+    exit 0;
+fi
+version=$(cat version.txt)
+revision=$(cat revision.txt)
+
+rm -Rf node-blue-node-ftp*
+mkdir node-blue-node-ftp-$version
+cp -R locales vendor *.php *.hni debian node-blue-node-ftp-$version
+date=`LANG=en_US.UTF-8 date +"%a, %d %b %Y %T %z"`
+echo "node-blue-node-ftp ($version-$revision) $1; urgency=low
+
+  * See https://forum.homegear.eu
+
+ -- Sathya Laufer <sathya@laufers.net>  $date" > node-blue-node-ftp-$version/debian/changelog
+tar -zcpf node-blue-node-ftp_$version.orig.tar.gz node-blue-node-ftp-$version
+cd node-blue-node-ftp-$version
+debuild -us -uc
+cd ..
+rm -Rf node-blue-node-ftp-$version
+mv node-blue-node-ftp* ..
---
+++ ftp/ftp.php
@@ -0,0 +1,44 @@
+<?php
+declare(strict_types=1);
+require_once('vendor/nicolab/php-ftp-client/FtpClient.php');
+require_once('vendor/nicolab/php-ftp-client/FtpException.php');
+require_once('vendor/nicolab/php-ftp-client/FtpWrapper.php');
+
+
+class HomegearNode extends HomegearNodeBase
+{
+    private $hg = NULL;
+    private $nodeInfo = NULL;
+
+    public function __construct()
+    {
+	    $this->hg = new \Homegear\Homegear();
+    }
+
+    public function input(array $nodeInfoLocal, int $inputIndex, array $message)
+    {
+        $config = $this->getConfigParameter($nodeInfoLocal['info']['server'],'port');
+        $ftp = new \FtpClient\FtpClient();
+        $ftp->connect($config['server']);
+        $ftp->login($config['username'], $config['password']);
+        $ftp->pasv(true);
+        
+        // check if directory exists
+        if (!$ftp->isDir($config['path'])) {
+            $ftp->mkdir($config['path']);
+        }
+
+        // change to directory
+        $ftp->chdir($config['path']);
+
+        // check if we have a path to a file
+        if ($nodeInfoLocal['info']['data'] == "filepath") {
+            // upload file
+            $ftp->putFromPath($message['payload']);
+        } elseif ($nodeInfoLocal['info']['data'] == "plaintext") {
+            // upload plaintext to a new file
+            $filename = time().".txt";
+            $ftp->putFromString($filename,$message['payload']);
+        }
+    }
+}

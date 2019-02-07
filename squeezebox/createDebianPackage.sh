0373d47cafec617e89463fa3331e3e8d98d22a5a
jboeddeker <jboeddeker@gmx.de>
2019-02-07 15:26:30 +0100

    added squeezebox node

---
+++ squeezebox/createDebianPackage.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+if test -z version.txt; then
+    echo "Please create a version file."
+    exit 0;
+fi
+if test -z revision.txt; then
+    echo "Please create a revision file."
+    exit 0;
+fi
+if test -z $1; then
+    echo "Please specify a distribution (i. e. wheezy)."
+    exit 0;
+fi
+version=$(cat version.txt)
+revision=$(cat revision.txt)
+
+rm -Rf node-blue-node-telegram*
+mkdir node-blue-node-telegram-$version
+cp -R locales *.php *.hni debian node-blue-node-telegram-$version
+date=`LANG=en_US.UTF-8 date +"%a, %d %b %Y %T %z"`
+echo "node-blue-node-telegram ($version-$revision) $1; urgency=low
+
+  * See https://forum.homegear.eu
+
+ -- Sathya Laufer <sathya@laufers.net>  $date" > node-blue-node-telegram-$version/debian/changelog
+tar -zcpf node-blue-node-telegram_$version.orig.tar.gz node-blue-node-telegram-$version
+cd node-blue-node-telegram-$version
+debuild -us -uc
+cd ..
+rm -Rf node-blue-node-telegram-$version
+if [ ! -d ../output ]; then
+	mkdir ../output
+fi
+mv node-blue-node-telegram* ../output/

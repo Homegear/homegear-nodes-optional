d6cfb8de3bee625b0dea14a207a35894970eaf81
jboeddeker <jboeddeker@gmx.de>
2019-02-08 19:51:53 +0100

    minor changes

--- squeezebox/squeezebox.php
+++ squeezebox/squeezebox.php
@@ -25,21 +25,24 @@ class HomegearNode extends HomegearNodeBase
         
         $this->_sendMessage($server,$port,$player,$p0,$p1,$p2,$p3,$p4);
     }
-
+    // GET request
     private function _sendMessage($server = false, $port = false, $player = false,$p0,$p1,$p2,$p3,$p4)
     {
         if ($server == false) { $this->log(4,"squeezebox: server missing!"); return; }
         if ($port == false) { $this->log(4,"squeezebox: port missing!"); return; }
         if ($player == false) { $this->log(4,"squeezebox: player missing!"); return; }
 
-        $url = "http://$server:$port/status.html?player=$player&p0=$p0&p1=$p1&p2=$p2&p3=$p3&p4=$p4";
-
+        //GET Request
+        $url = "http://$server:$port/status.html?p0=$p0&p1=$p1&p2=$p2&p3=$p3&p4=$p4&player=$player";
         $ch = curl_init($url);
         curl_setopt($ch, CURLOPT_HEADER, false);
         $result = curl_exec($ch);
-        curl_close($ch);
+        curl_close($ch); 
 
         $this->output(0, array('payload' => $result));
     }
+
+    
+
 }
 ?>

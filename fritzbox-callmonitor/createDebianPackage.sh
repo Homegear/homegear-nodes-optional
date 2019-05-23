52820102f2d9fcb6ab4b1761eb4d47ac6a388eeb
Joachim Boeddeker <jboeddeker@gmx.de>
2019-05-23 22:39:59 +0200

    initial fritzbox

---
+++ fritzbox-callmonitor/createDebianPackage.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+if test -z version.txt; then
+    echo "Please create a version file."
+    exit 0;
+fi
+if test -z revision.txt; then
+    echo "Please create a revision file."
+    exit 0;
+fi
+if test -z $1; then
+    echo "Please specify a distribution (i. e. wheezy)."
+    exit 0;
+fi
+version=$(cat version.txt)
+revision=$(cat revision.txt)
+
+rm -Rf node-blue-node-fritzbox-callmonitor*
+mkdir node-blue-node-fritzbox-callmonitor-$version
+cp -R locales *.png *.php *.hni debian node-blue-node-fritzbox-callmonitor-$version
+date=`LANG=en_US.UTF-8 date +"%a, %d %b %Y %T %z"`
+echo "node-blue-node-fritzbox-callmonitor ($version-$revision) $1; urgency=low
+
+  * See https://forum.homegear.eu
+
+ -- Sathya Laufer <sathya@laufers.net>  $date" > node-blue-node-fritzbox-callmonitor-$version/debian/changelog
+tar -zcpf node-blue-node-fritzbox-callmonitor_$version.orig.tar.gz node-blue-node-fritzbox-callmonitor-$version
+cd node-blue-node-fritzbox-callmonitor-$version
+debuild -us -uc
+cd ..
+rm -Rf node-blue-node-fritzbox-callmonitor-$version
+if [ ! -d ../output ]; then
+	mkdir ../output
+fi
+mv node-blue-node-fritzbox-callmonitor* ../output/

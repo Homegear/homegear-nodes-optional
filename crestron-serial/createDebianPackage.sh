0955092843824fbae58d7108fd7b12b30d3d3da6
Dr. Sathya Laufer <s.laufer@homegear.email>
2020-07-23 07:59:19 +0200

    Fixed: Debian package of crestron serial node not created correctly and added help

--- crestron-serial/createDebianPackage.sh
+++ crestron-serial/createDebianPackage.sh
@@ -11,12 +11,15 @@ if test -z $1; then
     echo "Please specify a distribution (i. e. wheezy)."
     exit 0;
 fi
+
+./bootstrap
+
 version=$(cat version.txt)
 revision=$(cat revision.txt)
 
 rm -Rf node-blue-node-crestron-serial*
 mkdir node-blue-node-crestron-serial-$version
-cp -R crestron-serial-in crestron-serial-out crestron-serial-port configure cfg m4 aclocal.m4 autom4te.cache config.h.in Makefile.in *.png debian *.md revision.txt version.txt *.sh LICENSE bootstrap configure.ac Makefile.am node-blue-node-crestron-serial-$version
+cp -R * node-blue-node-crestron-serial-$version
 date=`LANG=en_US.UTF-8 date +"%a, %d %b %Y %T %z"`
 echo "node-blue-node-crestron-serial ($version-$revision) $1; urgency=low
 
